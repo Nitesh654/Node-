3bf6ca57cc756537ab8f2f6e59b7231c494487cc
Nitesh Kumar <61279123+Nitesh654@users.noreply.github.com>
2024-05-18 16:24:06 +0530

    using express.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log(" another middleware");
+    res.send('<h1> hello to node js </h1>')
+    // res.send('{ key1: value }')
+});
+
+app.listen(3000);
