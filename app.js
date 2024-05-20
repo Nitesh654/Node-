0791d8d3762d62945d735f5721736fea1bd7df5e
Nitesh Kumar <61279123+Nitesh654@users.noreply.github.com>
2024-05-20 18:17:16 +0530

    body-parsing

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+    // console.log("middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log(" another middleware");
-    res.send('<h1> hello to node js </h1>')
+app.post('/product', (req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
+    // console.log("another middleware");
+    res.send('<h1> node js </h1>')
     // res.send('{ key1: value }')
 });
 
