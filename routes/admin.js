de1d8b4df837d1a67e5fb0ba3c3802eea652d9d0
Nitesh Kumar <nitesh_kumar@srmap.edu.in>
2024-06-06 12:32:55 +0530

    added routes for modified and remove products

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
+// GET - /add-product
 router.get('/add-product', adminController.getAddProduct);
 
 router.get('/admin-product', adminController.getAdminProducts);
@@ -13,5 +14,9 @@ router.get('/admin-product', adminController.getAdminProducts);
 router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditMyProduct);
+
+router.post('/edit-product', adminController.saveModifiedProduct);
+
+router.post('/remove-product', adminController.removeProduct);
 
 module.exports = router;
