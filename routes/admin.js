9b7b984fc3dab42706243cd5840272983f396145
Nitesh Kumar <nitesh_kumar@srmap.edu.in>
2024-06-06 11:16:35 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,17 @@
+const path = require('path');
+
 const express = require('express');
+
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
-router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="integer" name="size"><button type="submit">Submit</button></form>');
-});
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', (req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+router.get('/admin-product', adminController.getAdminProducts);
+
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditMyProduct);
+
 module.exports = router;
