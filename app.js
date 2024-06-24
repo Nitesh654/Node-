b2e4627e42e44bd8720cc1b11d077e268415fc88
Nitesh Kumar <nitesh_kumar@srmap.edu.in>
2024-06-24 18:26:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const http = require('http');
+// const http = require('http');
 const path = require('path');
+const sequelize = require('./connection/database');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -13,13 +14,43 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+}) 
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+// .sync({ force: true })
+.sync()
+.then(result => {
+    return User.findById(1);
+})
+.then(user => {
+    if(!user){
+        return User.create({ name: 'Max', email: 'max@gmail.com'})
+    }
+    return user;
+})
+.then()
+.catch((err) => console.log(err));
+
+
+app.listen(4000);
